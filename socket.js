ac9442a3a405c41453d5a9bd59c126b74f5099b9
robin <robin.prillwitz@gmail.com>
2024-08-04 17:49:56 +0200

    handle no image

--- socket.js
+++ socket.js
@@ -61,12 +61,12 @@ module.exports = (io, authenticateToken, database) => {
         database.getLatestImage(device_id, (err, rows, fields) => {
             if(err) {failureMessage(err);}
 
-            if(rows.length > 1) {
+            if(rows.length > 0) {
               const buffer = Buffer.from(rows[0].image_data);
               const timestamp = rows[0].timestamp;
               socket.emit('imageUpdate', { buffer: Array.from(buffer), timestamp });
             } else  {
-              failureMessage("no image");
+              failureMessage(`no image for ${device_id}`);
             }
         });
     });
